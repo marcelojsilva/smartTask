886b37d5fd714ba3208933280a6e4e07a10a9593
marcelojsilva <marcelojosedasilva@gmail.com>
2022-03-02 20:40:00 -0300

    Applied solhint and slither, script local deploy

--- smartContract/contracts/task.sol
+++ smartContract/contracts/task.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+pragma solidity 0.8.12;
 
 contract Task {
     enum TaskStatus {
@@ -38,92 +38,96 @@ contract Task {
 
     event SetTaskAsComplete(uint256 taskIndex);
 
-    modifier onlyOwner(uint256 _taskIndex) {
+    modifier onlyOwner(uint256 taskIndex) {
         require(
-            tasks[_taskIndex].wallet == msg.sender,
+            tasks[taskIndex].wallet == msg.sender,
             "only owner of the task"
         );
         _;
     }
 
-    modifier onlyValidIndex(uint256 _taskIndex) {
-        require(_taskIndex < tasks.length, "not a valid index");
-        require(tasks[_taskIndex].deleted == false, "task deleted");
+    modifier onlyValidIndex(uint256 taskIndex) {
+        require(taskIndex < tasks.length, "not a valid index");
+        require(!tasks[taskIndex].deleted, "task deleted");
         _;
     }
 
     function addTask(
-        uint256 _dateTimeStampInSeconds,
-        string memory _title,
-        string memory _description,
-        uint256 _priority
+        uint256 dateTimeStampInSeconds,
+        string memory title,
+        string memory description,
+        uint256 priority
     ) external returns (uint256 index) {
         require(
-            _priority >= 1 && _priority <= 3,
+            priority >= 1 && priority <= 3,
             "priority must be between 1 and 3"
         );
-        TaskStruct memory _task = TaskStruct({
-            date: _dateTimeStampInSeconds,
-            title: _title,
-            description: _description,
+        TaskStruct memory task = TaskStruct({
+            date: dateTimeStampInSeconds,
+            title: title,
+            description: description,
             wallet: msg.sender,
             status: TaskStatus.ToDo,
-            priority: _priority,
+            priority: priority,
             deleted: false
         });
-        tasks.push(_task);
+        tasks.push(task);
         index = tasks.length - 1;
         myTasks[msg.sender].push(index);
 
-        emit AddTask(_dateTimeStampInSeconds, _title, _description, _priority);
+        emit AddTask(dateTimeStampInSeconds, title, description, priority);
     }
 
     function editTask(
-        uint256 _taskIndex,
-        uint256 _dateTimeStampInSeconds,
-        string memory _title,
-        string memory _description,
-        uint256 _priority
-    ) external onlyValidIndex(_taskIndex) onlyOwner(_taskIndex) {
+        uint256 taskIndex,
+        uint256 dateTimeStampInSeconds,
+        string memory title,
+        string memory description,
+        uint256 priority
+    ) external onlyValidIndex(taskIndex) onlyOwner(taskIndex) {
         require(
-            _priority >= 1 && _priority <= 3,
+            priority >= 1 && priority <= 3,
             "priority must be between 1 and 3"
         );
-        TaskStruct storage task = tasks[_taskIndex];
+        TaskStruct storage task = tasks[taskIndex];
 
-        task.date = _dateTimeStampInSeconds;
-        task.title = _title;
-        task.description = _description;
-        task.priority = _priority;
+        task.date = dateTimeStampInSeconds;
+        task.title = title;
+        task.description = description;
+        task.priority = priority;
 
         emit EditTask(
-            _taskIndex,
-            _dateTimeStampInSeconds,
-            _title,
-            _description,
-            _priority
+            taskIndex,
+            dateTimeStampInSeconds,
+            title,
+            description,
+            priority
         );
     }
 
-    function removeTask(uint256 _taskIndex) external onlyValidIndex(_taskIndex) onlyOwner(_taskIndex) {
-        tasks[_taskIndex].deleted = true;
+    function removeTask(uint256 taskIndex)
+        external
+        onlyValidIndex(taskIndex)
+        onlyOwner(taskIndex)
+    {
+        tasks[taskIndex].deleted = true;
     }
 
-    function setTaskAsComplete(uint256 _taskIndex)
+    function setTaskAsComplete(uint256 taskIndex)
         external
-        onlyValidIndex(_taskIndex)
-        onlyOwner(_taskIndex)
+        onlyValidIndex(taskIndex)
+        onlyOwner(taskIndex)
     {
-        TaskStruct storage task = tasks[_taskIndex];
+        TaskStruct storage task = tasks[taskIndex];
         task.status = TaskStatus.Complete;
 
-        emit SetTaskAsComplete(_taskIndex);
+        emit SetTaskAsComplete(taskIndex);
     }
 
-    function getTask(uint256 _taskIndex)
+    function getTask(uint256 taskIndex)
         external
         view
-        onlyValidIndex(_taskIndex)
+        onlyValidIndex(taskIndex)
         returns (
             uint256 date,
             string memory title,
@@ -133,31 +137,31 @@ contract Task {
             uint256 priority
         )
     {
-        date = tasks[_taskIndex].date;
-        title = tasks[_taskIndex].title;
-        description = tasks[_taskIndex].description;
-        wallet = tasks[_taskIndex].wallet;
-        status = tasks[_taskIndex].status;
-        priority = tasks[_taskIndex].priority;
+        date = tasks[taskIndex].date;
+        title = tasks[taskIndex].title;
+        description = tasks[taskIndex].description;
+        wallet = tasks[taskIndex].wallet;
+        status = tasks[taskIndex].status;
+        priority = tasks[taskIndex].priority;
     }
 
     function listMyTasks() external view returns (TaskStruct[] memory) {
         uint256 myTotalTasks = myTasks[msg.sender].length;
-        uint256 totalValid;
-        
-        for (uint i = 0; i < myTotalTasks; i++) {
-            uint256 _taskIndex = myTasks[msg.sender][i];
-            if (!tasks[_taskIndex].deleted) {
+        uint256 totalValid = 0;
+
+        for (uint256 i = 0; i < myTotalTasks; i++) {
+            uint256 taskIndex = myTasks[msg.sender][i];
+            if (!tasks[taskIndex].deleted) {
                 totalValid++;
             }
         }
 
         TaskStruct[] memory _myTasks = new TaskStruct[](totalValid);
         uint256 count;
-        for (uint i = 0; i < myTotalTasks; i++) {
-            uint256 _taskIndex = myTasks[msg.sender][i];
-            if (!tasks[_taskIndex].deleted) {
-                _myTasks[count] = tasks[_taskIndex];
+        for (uint256 i = 0; i < myTotalTasks; i++) {
+            uint256 taskIndex = myTasks[msg.sender][i];
+            if (!tasks[taskIndex].deleted) {
+                _myTasks[count] = tasks[taskIndex];
                 count++;
             }
         }
@@ -168,23 +172,23 @@ contract Task {
     function listMyTodayTasks() external view returns (TaskStruct[] memory) {
         uint256 today = block.timestamp / 1 days;
         uint256 myTotalTasks = myTasks[msg.sender].length;
-        uint256 totalValid;
-        
-        for (uint i = 0; i < myTotalTasks; i++) {
-            uint256 _taskIndex = myTasks[msg.sender][i];
-            uint256 taskDay = tasks[_taskIndex].date / 1 days;
-            if (!tasks[_taskIndex].deleted  && taskDay == today) {
+        uint256 totalValid = 0;
+
+        for (uint256 i = 0; i < myTotalTasks; i++) {
+            uint256 taskIndex = myTasks[msg.sender][i];
+            uint256 taskDay = tasks[taskIndex].date / 1 days;
+            if (!tasks[taskIndex].deleted && taskDay == today) {
                 totalValid++;
             }
         }
 
         TaskStruct[] memory _myTasks = new TaskStruct[](totalValid);
         uint256 count;
-        for (uint i = 0; i < myTotalTasks; i++) {
-            uint256 _taskIndex = myTasks[msg.sender][i];
-            uint256 taskDay = tasks[_taskIndex].date / 1 days;
-            if (!tasks[_taskIndex].deleted  && taskDay == today) {
-                _myTasks[count] = tasks[_taskIndex];
+        for (uint256 i = 0; i < myTotalTasks; i++) {
+            uint256 taskIndex = myTasks[msg.sender][i];
+            uint256 taskDay = tasks[taskIndex].date / 1 days;
+            if (!tasks[taskIndex].deleted && taskDay == today) {
+                _myTasks[count] = tasks[taskIndex];
                 count++;
             }
         }
